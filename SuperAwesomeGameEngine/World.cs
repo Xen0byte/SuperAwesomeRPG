7d634323592208c171ddea9221a2e875d6f5e294
Vlad-Alexandru Tănăsescu <vlad_tanasescu@outlook.com>
2017-05-18 17:24:19 +0100

    Finished the World static class and added some controls to the form.

--- SuperAwesomeGameEngine/World.cs
+++ SuperAwesomeGameEngine/World.cs
@@ -52,6 +52,9 @@ namespace SuperAwesomeGameEngine
             PopulateQuests();
             PopulateLocations();
         }
+        //This is the static constructor.
+        //You might be thinking, "Wait! We can't instantiate a static class, so why does it have a constructor? After all, that's what a constructor is used for, instantiating an object!"
+        //With a static class, the constructor code is run the first time someone uses anything in the class.
 
         private static void PopulateItems()
         {
@@ -90,8 +93,7 @@ namespace SuperAwesomeGameEngine
         {
             Quest clearAlchemistGarden = new Quest(QUEST_ID_CLEAR_ALCHEMIST_GARDEN,
                 "Clear the alchemist's garden.",
-                "Kill rats in the alchemist's garden and bring back 3 rat tails. You will receive a healing potion and 10 gold pieces.",
-                20, 10);
+                "Kill rats in the alchemist's garden and bring back 3 rat tails. You will receive a healing potion and 10 gold pieces.", 20, 10);
 
             clearAlchemistGarden.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_RAT_TAIL), 3));
 
@@ -99,8 +101,7 @@ namespace SuperAwesomeGameEngine
 
             Quest clearFarmersField = new Quest(QUEST_ID_CLEAR_FARMERS_FIELD,
                 "Clear the farmer's field.",
-                "Kill snakes in the farmer's field and bring back 3 snake fangs. You will receive an adventurer's pass and 20 gold pieces.",
-                20, 20);
+                "Kill snakes in the farmer's field and bring back 3 snake fangs. You will receive an adventurer's pass and 20 gold pieces.", 20, 20);
 
             clearFarmersField.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_SNAKE_FANG), 3));
 
@@ -112,41 +113,41 @@ namespace SuperAwesomeGameEngine
 
         private static void PopulateLocations()
         {
-            //Create each location.
+            //Creating each location.
             Location home = new Location(LOCATION_ID_HOME, "Home",
-            "Your house. You really need to clean up the place.");
+                "Your house. You really need to clean up the place.");
 
             Location townSquare = new Location(LOCATION_ID_TOWN_SQUARE,
-            "Town square", "You see a fountain.");
+                "Town square", "You see a fountain.");
 
             Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT,
-            "Alchemist's hut", "There are many strange plants on the shelves.");
+                "Alchemist's hut", "There are many strange plants on the shelves.");
             alchemistHut.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_ALCHEMIST_GARDEN);
 
             Location alchemistsGarden = new Location(LOCATION_ID_ALCHEMISTS_GARDEN,
-            "Alchemist's garden", "Many plants are growing here.");
+                "Alchemist's garden", "Many plants are growing here.");
             alchemistsGarden.MonsterLivingHere = MonsterByID(MONSTER_ID_RAT);
 
             Location farmhouse = new Location(LOCATION_ID_FARMHOUSE,
-            "Farmhouse", "There is a small farmhouse, with a farmer in front.");
+                "Farmhouse", "There is a small farmhouse, with a farmer in front.");
             farmhouse.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_FARMERS_FIELD);
 
             Location farmersField = new Location(LOCATION_ID_FARM_FIELD,
-            "Farmer's field", "You see rows of vegetables growing here.");
+                "Farmer's field", "You see rows of vegetables growing here.");
             farmersField.MonsterLivingHere = MonsterByID(MONSTER_ID_SNAKE);
 
             Location guardPost = new Location(LOCATION_ID_GUARD_POST,
-            "Guard post", "There is a large, tough-looking guard here.",
-            ItemByID(ITEM_ID_ADVENTURER_PASS));
+                "Guard post", "There is a large, tough-looking guard here.",
+                ItemByID(ITEM_ID_ADVENTURER_PASS));
 
             Location bridge = new Location(LOCATION_ID_BRIDGE,
-"Bridge", "A stone bridge crosses a wide river.");
+                "Bridge", "A stone bridge crosses a wide river.");
 
             Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD,
-            "Forest", "You see spider webs covering covering the trees in this forest.");
+                "Forest", "You see spider webs covering the trees in this forest.");
             spiderField.MonsterLivingHere = MonsterByID(MONSTER_ID_GIANT_SPIDER);
 
-            //Link the locations together.
+            //Linking the locations together.
             home.LocationToNorth = townSquare;
 
             townSquare.LocationToNorth = alchemistHut;
@@ -172,7 +173,7 @@ namespace SuperAwesomeGameEngine
 
             spiderField.LocationToWest = bridge;
 
-            //Add the locations to the static list.
+            //Adding the locations to the static list.
             Locations.Add(home);
             Locations.Add(townSquare);
             Locations.Add(guardPost);
@@ -182,6 +183,54 @@ namespace SuperAwesomeGameEngine
             Locations.Add(farmersField);
             Locations.Add(bridge);
             Locations.Add(spiderField);
+        }
+
+        public static Item ItemByID(int id)
+        {
+            foreach (Item item in Items)
+            {
+                if (item.ID == id)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public static Monster MonsterByID(int id)
+        {
+            foreach (Monster monster in Monsters)
+            {
+                if (monster.ID == id)
+                {
+                    return monster;
+                }
+            }
+            return null;
+        }
+
+        public static Quest QuestByID(int id)
+        {
+            foreach (Quest quest in Quests)
+            {
+                if (quest.ID == id)
+                {
+                    return quest;
+                }
+            }
+            return null;
+        }
+
+        public static Location LocationByID(int id)
+        {
+            foreach (Location location in Locations)
+            {
+                if (location.ID == id)
+                {
+                    return location;
+                }
+            }
+            return null;
         }
     }
 }
